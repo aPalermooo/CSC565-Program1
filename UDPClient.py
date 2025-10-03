3b13658e9d878f601e6740a355836542eef36ff1
aPalermooo <ajp2s@login.missouristate.edu>
2025-10-03 01:22:27 -0500

    init UDP functionality

---
+++ UDPClient.py
@@ -0,0 +1,19 @@
+from socket import *
+
+# TODO:
+# THREAD
+
+def main():
+    server_name = 'localhost'
+    server_port = 12000
+    with socket(AF_INET, SOCK_DGRAM) as CLIENT_SOCKET:
+        message = input('Input lowercase sentence:')
+        CLIENT_SOCKET.sendto(message.encode(),\
+                                    (server_name, server_port))
+        modified_message, server_address = \
+                                    CLIENT_SOCKET.recvfrom(2048)
+        print(modified_message.decode())
+        CLIENT_SOCKET.close()
+
+if __name__ == "__main__":
+    main()
---
+++ UDPServer.py
@@ -0,0 +1,16 @@
+from socket import *
+
+
+def main():
+    server_port = 12000
+    with socket(AF_INET, SOCK_DGRAM) as SERVER_SOCKET:
+        SERVER_SOCKET.bind(('', server_port))
+        print ("The server is ready to receive")
+        while True:
+            message, client_address = SERVER_SOCKET.recvfrom(2048)
+            modified_message = message.decode().upper()
+            SERVER_SOCKET.sendto(modified_message.encode(),\
+                                                            client_address)
+
+if __name__ == "__main__":
+    main()
