c8719b6178a45c83a4d24534448fabb423f2f52b
aPalermooo <ajp2s@login.missouristate.edu>
2025-10-03 14:40:56 -0500

    Complete create_message

--- share/ClientFunction.py
+++ share/ClientFunction.py
@@ -1,3 +1,6 @@
+from decimal import InvalidOperation
+from os import WCONTINUED
+
 from share.Message import Message
 
 DIVIDER = '\n' + ('-' * 30) + '\n'
@@ -14,6 +17,11 @@ def create_message(connection_type : str) -> Message:
     :param connection_type: Text string that indicates function of program
     :return: Message Object containing user inputs
     """
+
+    def reject_input(inp : str, reason : str) :
+        print(f"\nInput invalid ({reason})"
+              f"\nYou entered: {inp}"
+              f"\nplease try again...\n{DIVIDER}")
 
     def prompt_type() -> int:
 
@@ -34,17 +42,13 @@ def create_message(connection_type : str) -> Message:
             user_input_dirty = input("Input:")
 
             if not user_input_dirty.isdigit() or user_input_dirty == "":
-                print(f"Input invalid (not a number)"
-                      f"\nYou entered: {user_input_dirty}"
-                      f"\nplease try again...{DIVIDER}")
+                reject_input(user_input_dirty, "not a number")
                 continue
 
             user_input = int(user_input_dirty)
 
             if user_input not in range(len(OPTIONS)):
-                print(f"Input invalid (not in range)"
-                      f"\nYou entered: {message_type}"
-                      f"\nplease try again...{DIVIDER}")
+                reject_input(user_input_dirty, "not in range")
                 continue
 
             break #Input valid
@@ -90,6 +94,42 @@ def create_message(connection_type : str) -> Message:
                     continue
         return user_input
 
+    def prompt_value(m_type : int, metric : bool ) -> float:
+        user_input = 0 # set in scope
+        while True:
+            options_list = list(OPTIONS.values())
+            options_list = options_list[m_type]
+
+            current_units = options_list[0] if metric else options_list[1]
+            future_units = options_list[1] if metric else options_list[0]
+
+            print(f"Input Number to be converted from {current_units} to {future_units}")
+            print(f"\t (Input e to exit Program)", end="\t")
+
+            if m_type == 3: #is_Temperature
+                print("(Can be negative)",end="\t")
+
+            print()
+
+            user_input_dirty = input("Input:")
+
+            if user_input_dirty == "e":
+                print("Terminating...")
+                exit(0)
+
+            try:
+                user_input = float(user_input_dirty)
+            except InvalidOperation:
+                reject_input(user_input_dirty, "not a number")
+                continue
+
+            if m_type != 3 and user_input <= 0:
+                reject_input(user_input_dirty, "non-integer number for this type of conversion")
+                continue
+
+            break #input valid
+        return user_input
+
     print(f"Starting Client...\n\t(Connection Type: {connection_type}){DIVIDER}")
 
     """CONVERSION TYPE"""
@@ -102,17 +142,14 @@ def create_message(connection_type : str) -> Message:
     is_metric = prompt_direction(message_type)
     print(DIVIDER)
 
+    """CONVERSION VALUE"""
 
+    value = prompt_value(message_type, is_metric)
+    print(DIVIDER)
 
+    message = Message((message_type, is_metric), value)
 
-
-    #TODO Prompt User for Value
-        #Check if is negative !! (if not temp)
-
-    #TODO: Return message to caller
-
-
-
+    return message
 
 
 def return_message(message : Message):
